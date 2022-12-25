bb17edf7ffff018310723f5f8df5d740cc03aa0a
lel <88410471+Iamlel@users.noreply.github.com>
2022-12-24 20:45:37 -0800

    Create part2.py

---
+++ day5/part2.py
@@ -0,0 +1,9 @@
+seats = []
+for line in open("input.txt", "r"):
+	seats.append(int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2))
+
+for seat in seats:
+    if not (seat + 1) in seats:
+        if (seat + 2) in seats:
+            print(seat + 1)
+            exit()
