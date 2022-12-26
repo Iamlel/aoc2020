ffdfa2081e603307ba6b9f4398a14af6d27cf717
lel <88410471+Iamlel@users.noreply.github.com>
2022-12-26 11:10:44 -0800

    use compile instead

--- day2/part1.py
+++ day2/part1.py
@@ -1,9 +1,8 @@
-# regex is slow but I wanted to use it
 import re
 
 valid = 0
 for line in open("input.txt", "r"):
-    password = re.split("[- :]+", line)
+    password = re.compile("[- :]+".split(line)
     if (int(password[0]) <= password[3].count(password[2]) <= int(password[1])):
         valid += 1
 print(valid)
