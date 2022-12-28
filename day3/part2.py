bfad0e53115280cdecd573b6e3343abe00df52a3
lel <88410471+Iamlel@users.noreply.github.com>
2022-12-27 17:31:32 -0800

    Update part2.py

--- day3/part2.py
+++ day3/part2.py
@@ -1,16 +1,14 @@
 import numpy
 
 file = open("input.txt", "r")
-#CHARCOUNT = 31
 SLOPES = ((1, 1), (1, 3), (1, 5), (1, 7), (2, 1))
 
 slopes = [0] * len(SLOPES)
-
-file.readline()
+charscount = len(file.readline()) - 1
 for k, line in enumerate(file):
 	for i, s in enumerate(SLOPES):
 		if ((k + 1) % s[0] == 0):
-			if (line[int((k + 1) * s[1] / s[0]) % 31] == '#'): # CHARCOUNT
+			if (line[int((k + 1) * s[1] / s[0]) % charscount] == '#'):
 				slopes[i] += 1
 
 print(numpy.product(slopes))
