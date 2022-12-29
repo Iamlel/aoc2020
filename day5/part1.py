a8d8dab1e28567fd998ab27896d8e0cc195ffffd
lel <88410471+Iamlel@users.noreply.github.com>
2022-12-28 20:22:01 -0800

    Update part1.py

--- day5/part1.py
+++ day5/part1.py
@@ -1,6 +1,7 @@
 highestID = 0
-for line in open("input.txt", "r"):
-	ID =  int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2)
-	if (ID > highestID):
-		highestID = ID
+with open("input.txt", "r") as fp:
+	for line in fp.readlines():
+		ID =  int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2)
+		if (ID > highestID):
+			highestID = ID
 print(highestID)
