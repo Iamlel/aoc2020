276317b5419f894d0495d88530a06c141893442e
lel <88410471+Iamlel@users.noreply.github.com>
2022-12-28 20:27:12 -0800

    Update part1.py

--- day4/part1.py
+++ day4/part1.py
@@ -1,12 +1,12 @@
 cpass, valid = 0, 0
 with open("input.txt", "r") as fp:
 	for line in fp.readlines():
-        	if (line[0] == '\n'):
-            		if (cpass == 7):
-                		valid += 1
-            		cpass = 0
-        	else:
-            		cpass += line.count(':') - line.count("cid")
+        if (line[0] == '\n'):
+            if (cpass == 7):
+                valid += 1
+            cpass = 0
+        else:
+            cpass += line.count(':') - line.count("cid")
 if (cpass == 7):
     print(valid + 1)
 else:
