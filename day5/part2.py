5578349055e169ef80163bd36cbcf2cb09af9beb
lel <88410471+Iamlel@users.noreply.github.com>
2022-12-28 20:22:25 -0800

    Update part2.py

--- day5/part2.py
+++ day5/part2.py
@@ -1,6 +1,7 @@
 seats = []
-for line in open("input.txt", "r"):
-	seats.append(int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2))
+with open("input.txt", "r") as fp:
+	for line in fp.readlines():
+		seats.append(int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2))
 
 for seat in seats:
     if not (seat + 1) in seats:
