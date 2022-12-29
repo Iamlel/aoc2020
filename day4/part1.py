0627d6474cffc7451dd5bbb4b4465685f330d548
lel <88410471+Iamlel@users.noreply.github.com>
2022-12-28 20:27:26 -0800

    Update part1.py

--- day4/part1.py
+++ day4/part1.py
@@ -1,6 +1,6 @@
 cpass, valid = 0, 0
 with open("input.txt", "r") as fp:
-	for line in fp.readlines():
+    for line in fp.readlines():
         if (line[0] == '\n'):
             if (cpass == 7):
                 valid += 1
