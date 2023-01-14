9ee8911e109b4f1902481170e0c01f13f0466e63
lel <88410471+Iamlel@users.noreply.github.com>
2023-01-14 11:55:03 -0800

    Update part1.py

--- day2/part1.py
+++ day2/part1.py
@@ -4,7 +4,7 @@ import re
 valid = 0
 with open("input.txt", "r") as fp:
 	for line in fp.readlines():
-        password = re.split("[- :]+", line)
-        if (int(password[0]) <= password[3].count(password[2]) <= int(password[1])):
-            valid += 1
+		password = re.split("[- :]+", line)
+		if (int(password[0]) <= password[3].count(password[2]) <= int(password[1])):
+			valid += 1
 print(valid)
